25a46cf1579d1337c39fb568cac4948998d25479
Anuj <anuj@Anujs-MacBook-Air.local>
2025-09-06 14:04:21 +0530

    feat(app): initial

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+if (process.env.NODE_ENV === 'development') {
+  import('@opennextjs/cloudflare').then(({ initOpenNextCloudflareForDev }) => {
+    initOpenNextCloudflareForDev();
+  }).catch(() => {
+    // Fail silently if the import fails
+  });
+}
+
+export default nextConfig;
