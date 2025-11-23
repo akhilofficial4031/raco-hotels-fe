dea24d86d1958f884525d493c0b91a8f841e2b2b
akhil mohandas <red10xofficial@gmail.com>
2025-11-23 16:18:19 +0530

    design fixes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,11 +14,14 @@ const Hero = ({ content }: HeroProps) => {
   const { openModal } = useQuickBooking();
   return (
     <section className="bg-background-light" aria-label="Hero section">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:pr-0 sm:pl-6 lg:pl-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div className="text-center md:text-left py-12 md:py-24">
             <AnimatedContainer animationName="fadeUp" delay={0.1}>
-              <p className="text-lg font-dm-sans uppercase tracking-widest text-primary" role="doc-subtitle">
+              <p
+                className="text-lg font-dm-sans uppercase tracking-widest text-primary"
+                role="doc-subtitle"
+              >
                 {content.tagline}
               </p>
             </AnimatedContainer>
@@ -53,7 +56,7 @@ const Hero = ({ content }: HeroProps) => {
             </AnimatedContainer>
           </div>
 
-          <div className="relative h-64 md:h-auto">
+          <div className="relative h-full">
             <Image
               src={content.image.src}
               alt={content.image.alt}
